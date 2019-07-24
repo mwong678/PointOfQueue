bc0b0fcb56d6db4cb6fa937b91ba2e9550c445d6
ctocicd <ctocicd@intuit.com>
2019-07-24 14:21:25 -0700

    stuff

--- app.js
+++ app.js
@@ -1082,6 +1082,7 @@ function updateQueues() {
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.PORT) {
+    console.log('redirecting...');
     return res.redirect('https://' + req.get('host') + req.url);
   }
   next();
