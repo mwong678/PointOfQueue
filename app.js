a8c4911a705a814375d9c4a57dc38544ad5d1f68
ctocicd <ctocicd@intuit.com>
2019-08-06 20:03:30 -0700

    deploy to heroku

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ mongo.connectToServer(function(err, client) {
  roomCollection.find().toArray((err, items) => {
   console.log(items);
  });
+ 
  setInterval(async () => {await background.updateQueues()}, 1000);
  app.listen(port, () => console.log('Listening on ' + port));
 });
